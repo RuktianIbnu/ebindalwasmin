4dfc0c19d592429e2890556df633a19e0b701fd3
RuktianIbnu <ruktianibnu95@gmail.com>
2020-09-17 13:38:22 +0700

    commit ke 6

--- lib/Screens/Beranda/drawerpage.dart
+++ lib/Screens/Beranda/drawerpage.dart
@@ -1,4 +1,4 @@
-import 'package:eBindalwasmin/Screens/Beranda/home.dart';
+import 'package:eBindalwasmin/Screens/Beranda/home3.dart';
 import 'package:eBindalwasmin/Screens/Report/laporan.dart';
 import 'package:flutter/material.dart';
 import 'package:eBindalwasmin/Screens/EntryData/components/Paspor.dart';
